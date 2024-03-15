d9257b2d5d3864a72edb5b3fe1e7d71f1952882b
KevinCannizzaro <skylar74@outlook.fr>
2024-03-15 16:11:36 +0100

    Step 01 : Back to the future

---
+++ exo.js
@@ -0,0 +1,39 @@
+// Step 01
+
+const couverts = [
+    'fourchette',
+    'couteau',
+    'cuillère',
+    'verre',
+    'assiette',
+];
+
+console.log(converts):
+
+const day = [
+    'monday',
+    'tuesday',
+    'wednesday'
+];
+
+console.log(days);
+
+const entrées = [
+    'salade césar',
+    'oeuf mimosa',
+    'tomates mozzarella'
+];
+
+const platsDeResistance = [
+    'Boeuf bourguignon',
+    'Risoto',
+    'Blanquette de dinde'
+];
+
+console.log("plats de résistance","platsDeResistance");
+
+const desserts = [
+    'yaourt sucré',
+    'gateau au chocolat',
+    'tarte au citrons'
+];
